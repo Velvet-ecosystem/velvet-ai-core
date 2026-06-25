971a400c3fcbaeee74be09cf958f5ec9cac9e8e5
Overlandranger <36721943+Overlandranger@users.noreply.github.com>
2026-06-25 12:01:13 -0700

    Export memory salience policy

--- velvet/core/memory/__init__.py
+++ velvet/core/memory/__init__.py
@@ -3,14 +3,17 @@
 from .associations import MemoryAssociationIndex
 from .bridge import MemoryTransitionBridge
 from .confidence import ConfidenceAssessment, MemoryConfidencePolicy
+from .decay import DecayAssessment, MemoryDecayPolicy
 from .deduplication import DuplicateGroup, MemoryDuplicateDetector
 from .lifecycle import MemoryLifecycle, MemoryTransition
 
 __all__ = [
     "ConfidenceAssessment",
+    "DecayAssessment",
     "DuplicateGroup",
     "MemoryAssociationIndex",
     "MemoryConfidencePolicy",
+    "MemoryDecayPolicy",
     "MemoryDuplicateDetector",
     "MemoryLifecycle",
     "MemoryTransition",
