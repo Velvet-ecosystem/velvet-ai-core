6960674aae932c70ab59864158c7dc461e047787
Overlandranger <36721943+Overlandranger@users.noreply.github.com>
2026-06-25 11:19:14 -0700

    Export memory association index

--- velvet/core/memory/__init__.py
+++ velvet/core/memory/__init__.py
@@ -1,6 +1,12 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
+from .associations import MemoryAssociationIndex
 from .bridge import MemoryTransitionBridge
 from .lifecycle import MemoryLifecycle, MemoryTransition
 
-__all__ = ["MemoryLifecycle", "MemoryTransition", "MemoryTransitionBridge"]
+__all__ = [
+    "MemoryAssociationIndex",
+    "MemoryLifecycle",
+    "MemoryTransition",
+    "MemoryTransitionBridge",
+]
