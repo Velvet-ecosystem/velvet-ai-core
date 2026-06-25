5ea781823448b90df5fdcebdebf3cbb891df5126
Overlandranger <36721943+Overlandranger@users.noreply.github.com>
2026-06-25 11:43:53 -0700

    Export memory confidence policy

--- velvet/core/memory/__init__.py
+++ velvet/core/memory/__init__.py
@@ -2,12 +2,15 @@
 
 from .associations import MemoryAssociationIndex
 from .bridge import MemoryTransitionBridge
+from .confidence import ConfidenceAssessment, MemoryConfidencePolicy
 from .deduplication import DuplicateGroup, MemoryDuplicateDetector
 from .lifecycle import MemoryLifecycle, MemoryTransition
 
 __all__ = [
+    "ConfidenceAssessment",
     "DuplicateGroup",
     "MemoryAssociationIndex",
+    "MemoryConfidencePolicy",
     "MemoryDuplicateDetector",
     "MemoryLifecycle",
     "MemoryTransition",
